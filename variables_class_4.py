5c07e2c059b8b364a5b8d6f8639ef1340a177b8b
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-05-20 17:19:54 +0300

    Add files via upload

---
+++ variables_class_4.py
@@ -0,0 +1,16 @@
+'''Найдите 12 примеров имён в вашем коде, 
+которые следует избегать, исправьте, и выложите на гитхаб 
+в формате "было - стало" (с учётом контекста).'''
+
+list0 - different_types_list # список с данными разных типов
+analit - product_analytics_dict # словарь с данными по продуктовой аналитике
+x1 - divisible_int # делимое
+x2 - divisor_int # делитель
+div - quotient # частное
+x1 - first_name # имя клиента
+x2 - last_name # фамилия клиента
+x3 - year_of_birth # год рождения
+x4 - city_of_resident # город проживания
+x5 - e_mail # e-mail
+x6 - phone_number # номер телефона
+my_sum - general_client_information # общие данные о клиенте
