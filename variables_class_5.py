77e2b9f0136e1320ede891079254a2bf3cef0e5b
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-05-26 09:55:31 +0300

    Add files via upload

---
+++ variables_class_5.py
@@ -0,0 +1,19 @@
+'''3.1. Улучшите пять имён классов в вашем коде.''''
+
+class Data_Preprocess - class Preprocess
+class Generate_features - class FeatureGenetator
+class Data_transformation - class Transform
+class Creat_card - class Card
+class Creat_number - class Number
+class Game_starting - class Game
+
+
+'''3.2. Улучшите семь имён методов и объектов по схеме из пункта 2.'''
+
+def change_card  - def change_card_for_no # используем метод только при ответе "д"
+def change_card2  - def change_card_for_yes # используем метод, при ответе "н"
+def delete(self, i): - def delete_index(self, index): # '''Удаляем объект в позиции i'''
+def Result(self): - def calculation_of_fabric_volume(self): # расчет объема ткани
+def my_func(self, thick): - def calculation_of_mass(self, thick): # расчёт массы асфальта
+def get_1(self): - def get_full_name(self): # получение полного имени сотрудника
+def get_2(self): - def get_total_income(self): # получение дохода с учетом премии
