d8c9676e0f01351a69f54d6b645066a7cbe8709f
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-05-29 00:27:23 +0300

    Add files via upload

---
+++ variables_class_6.py
@@ -0,0 +1,16 @@
+'''Выполните 12 улучшений имён функций/методов в вашем коде в формате "было - стало - ваш комментарий", 
+и выложите на гитхаб.'''
+
+
+def date_mistake - def replacing_outlier_to_average_value # замена выбросов на среднее значение
+def lemmatize -  def  get_word_formal_form # '''Лемматизация, получим нормальную (начальную) форму слова'''
+def vector_row_1 -  def fill_empty_lines# '''Заполним пустые строки'''
+def vector_row_2 -  def  get_tokens_of_text_field# '''Получим токены текстового поля'''
+def vector_row_3 -  def get_row_formal_form # '''Лемматизация , получим нормальную (начальную) форму слова (именительный падеж)'''
+def vector_row_4 -  def  remov_stop_words# '''Убираем стоп-слова'''
+def vector_row_5 -  def model_training # '''Обучение модели'''
+def vector_row_average -  def  calculate_vector_average_value# '''Вычисляем среднее значение для вектора'''
+def text_rows_nlp_vector -  def  cycle_for_text_fields# '''Метод цикла для обработки текстовых полей
+def text_row_nlp_vector-  def  text_field_processing# '''Метод для обработки текстового поля'''
+def row_worker -  def  applying_def_for_fields_list#'''применение метода для перечня полей'''
+def category_change -  def  processing_categorical_row#'''Метод обработки категориальных данных'''
