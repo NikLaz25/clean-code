6ac3786a109436ad6a6bedd9645eeebbf992ddf8
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-05-31 20:15:17 +0300

    Add files via upload

---
+++ variables_class_7.py
@@ -0,0 +1,3 @@
+def create_document - def  docum_type_definition #определение типа документа
+def _get_serializer - def detect_ files_type #  определить тип файла
+def create_shape - def detect_objects_shape #определить форму предмета
